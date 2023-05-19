9ffb9b0239a38c1e31478e379d9d79ace487d5ca
unknown <mochobarrera12@gmail.com>
2023-05-18 19:54:52 -0500

    hola

---
+++ app/src/main/java/com/example/reinosojeffersonejemplo2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.reinosojeffersonejemplo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText rtret;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
